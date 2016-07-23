61bc45b26608738881009729e0769990b8dcb3dc
misdcsp368 <3rdyearacount@Comlab201.local>
2016-07-23 14:00:07 +0800

    Updated JS Activity

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
+var _title = document.getElementById('myTitle');
+_title.innerHTML = "My New Title";
 
-	document.getElementById('titleheading').innerHTML = "JavaScript Title Heading Here";
-	document.getElementById('subtitle').innerHTML= "JavaScript Subtitle Here";
-	document.getElementById('samptbl').innerHTML= "<tr><th>Name</th><th>Gender</th></tr>" +
-												  "<tr><td>Juan Dela Cruz</td><td>Male</td></tr>" +
-												  "<tr><td>Rodrigo Duterte</td><td>Male</td></tr>" +
-												  "<tr><td>Bato Dela Rosa</td><td>Male</td></tr>";
+var _subtitle = document.getElementById('mySubTitle');
+_subtitle.innerHTML = "My New SubTitle";
 
+var _table = document.getElementById('myTable');
+var row = 3;
+var col = 1;
+var arrNames = ['Reymar', 'Cindy', 'Kevin'];
+var arrGender = ['Male', 'Female', 'Male'];
+var index = 0;
+var html = "<thead><tr><th>Name</th><th>Gender</th></tr></thead>";
+html += "<tbody>";
+for(x=0;x<row;x++){
+  html+="<tr>";
+  for(y=0;y<col;y++){
+  html+="<td>"+arrNames[index]+"</td>";
+  html+="<td>"+arrGender[index]+"</td>";
+  }
+  html+="</tr>";
+  index++;
+}
+html += "</tbody>";
+_table.innerHTML = html;
