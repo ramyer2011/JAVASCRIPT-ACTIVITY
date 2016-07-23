074b693b22ff4de1336a9178652c10fb45923841
misdcsp368 <3rdyearacount@Comlab201.local>
2016-07-23 13:20:36 +0800

    My first JavaScript Acitivity

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+	document.getElementById('titleheading').innerHTML = "JavaScript Title Heading Here";
+	document.getElementById('subtitle').innerHTML= "JavaScript Subtitle Here";
+	document.getElementById('samptbl').innerHTML= "<tr><th>Name</th><th>Gender</th></tr>" +
+												  "<tr><td>Juan Dela Cruz</td><td>Male</td></tr>" +
+												  "<tr><td>Rodrigo Duterte</td><td>Male</td></tr>" +
+												  "<tr><td>Bato Dela Rosa</td><td>Male</td></tr>";
+
