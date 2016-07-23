ee4bd54d03f835423dd5b57ee63977860ad3bd91
misdcsp368 <3rdyearacount@Comlab201.local>
2016-07-23 14:04:58 +0800

    Updated JS Activity

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ _subtitle.innerHTML = "My New SubTitle";
 
 var _table = document.getElementById('myTable');
 var row = 3;
-var col = 1;
+var col = 2;
 var arrNames = ['Reymar', 'Cindy', 'Kevin'];
 var arrGender = ['Male', 'Female', 'Male'];
 var index = 0;
@@ -14,10 +14,8 @@ var html = "<thead><tr><th>Name</th><th>Gender</th></tr></thead>";
 html += "<tbody>";
 for(x=0;x<row;x++){
   html+="<tr>";
-  for(y=0;y<col;y++){
   html+="<td>"+arrNames[index]+"</td>";
   html+="<td>"+arrGender[index]+"</td>";
-  }
   html+="</tr>";
   index++;
 }
